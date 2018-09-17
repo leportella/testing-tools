224bd3f73d8b622ece2598c9f917b573d597813f
Leticia Portella <leportella@gmail.com>
2018-09-17 19:53:25 -0300

    add models of student and parent

--- school/student/models.py
+++ school/student/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Parent(models.Model):
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+
+
+class Student(models.Model):
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    address = models.CharField(max_length=300)
+    resume = models.TextField()
+    age = models.PositiveSmallIntegerField()
+    email = models.CharField(max_length=250)
+    date_started = models.DateTimeField()
+    gender = models.CharField(max_length=200)
+    parent = models.ForeignKey(Parent, related_name='children',
+                               on_delete=models.CASCADE)
