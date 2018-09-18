c29aa6e58a8c09cd39d6498d02a35e19d415294a
Leticia Portella <leportella@gmail.com>
2018-09-18 12:30:02 -0300

    add fixture

--- school/student/tests/tests.py
+++ school/student/tests/tests.py
@@ -4,11 +4,14 @@ import pytest
 
 from .factories import StudentFactory
 
+@pytest.fixture
+def user():
+    return StudentFactory()
+
 
 @pytest.mark.django_db
-def test_endpoint(client):
-    student = StudentFactory()
-    response = client.get(f'/api/{student.id}', follow=True)
+def test_endpoint(client, user):
+    response = client.get(f'/api/{user.id}', follow=True)
     content = json.loads(response.content)
     assert response.status_code == 200
     assert content['name'] == student.first_name
